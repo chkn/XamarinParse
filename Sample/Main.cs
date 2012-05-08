a289aa89f5ffac87bc95db83e6589d2dfccd57c6
Alex Corrado <alexander.corrado@gmail.com>
2012-05-07 20:17:16 -0400

    Odds and ends

--- Sample/Main.cs
+++ Sample/Main.cs
@@ -1,5 +1,6 @@
 using System;
 
+using Cirrus;
 using Xamarin.Parse;
 
 namespace Sample {
@@ -11,10 +12,18 @@ namespace Sample {
 		public static void Main (string[] args)
 		{
 			Parse.Initialize (APP_ID, API_KEY, TimeSpan.FromSeconds (10));
+			Go ();
+			Thread.Current.RunLoop ();
+		}
+
+		public void Go ()
+		{
 			var testObject = new ParseObject ("TestObject");
 			testObject ["foo"] = "bar";
 			testObject ["baz"] = 10;
 			testObject.Save().Wait();
+			Console.WriteLine ("Saved: {0}", testObject.ObjectId);
+			Environment.Exit (0);
 		}
 	}
 }
